414ba9e61fb7ff9e2896176cd139f13c534ab8a4
Max Lauster <max.lauster@gmail.com>
2022-05-10 19:43:27 +0000

    yfkl

--- shiny_app/app.R
+++ shiny_app/app.R
@@ -3,6 +3,96 @@ library(leaflet)
 library(dplyr)
 library(lubridate)
 
+#Install Packages for Leaflet %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+knitr::opts_chunk$set(echo = TRUE)
+
+# Store string containing all required packages
+my_packages <- c('bipartite', 'RColorBrewer')
+# Store all installed packages
+ya_installed <- library()$results[,1]
+
+# Check whether required packages are already installed and grab only those that still need installation
+need_install<-my_packages[!(my_packages %in% ya_installed)]
+
+#install required packages
+lapply({need_install}, install.packages, character.only = TRUE)
+# Store all installed packages
+ya_loaded <- (.packages())
+
+# Check whether required packages are already installed and grab only those that still need installation
+need_load<-my_packages[!(my_packages %in% ya_loaded)]
+
+# Load required packages
+lapply(need_load, require, character.only = TRUE)
+
+#install.packages("xaringanthemer")
+library(tidyverse)
+library(tidymodels)
+#library(palmerpenguins)
+library(knitr)
+library(xaringanthemer)
+#install.packages("maps")
+#install.packages("corrplot")
+
+library(maps)
+library(ggplot2)
+library(tidyverse)
+library(corrplot)
+
+library(dplyr)
+#library(dsbox)
+library(skimr)
+library(robotstxt)
+library(tidyr) 
+library(leaflet) ## For leaflet interactive maps
+library(sf) ## For spatial data
+library(RColorBrewer) ## For colour palettes
+library(htmltools) ## For html
+library(leafsync) ## For placing plots side by side
+library(kableExtra) ## Table output
+#install.packages("geojsonio")
+#install.packages("rgdal")
+#install.packages("leaflet")
+library(leaflet)
+#install.packages("geojsonio")
+library(geojsonio)
+library(sf)
+library(leaflet)
+#install.packages("sf")
+library(RColorBrewer)
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+#READ IN JSON FOR STATE DATA%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+states <- geojsonio::geojson_read(x = "https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states.json"
+                                  , what = "sp")
+class(states)
+
+library(readr)
+states_all <- read.csv("all_parole_years.csv")
+
+states <- st_as_sf(states)
+states <- states %>%
+  mutate(name = str_to_lower(name))
+
+parole_df <- parole_df %>%
+  mutate(state = str_to_lower(state))
+
+state_parole <- parole_df %>%
+  left_join(states, by = c("state"="name")) %>%
+  st_as_sf()
+
+state_parole <- state_parole %>%
+  filter(year=1995)
+
+#this is for changing color 
+bins <- c(0, 100, 200, 300, 400, 500, 600, 700, 800, 900)
+pal <- colorBin("Spectral", domain = state_parole$number_on_parole_per_100000_us_adult_residents, bins = bins)
+
+labels <- sprintf("<strong>%s</strong><br/>%g Parole/100000 US Adults",
+                  state_parole$state, state_parole$number_on_parole_per_100000_us_adult_residents) %>% lapply(htmltools::HTML)
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
@@ -32,7 +122,32 @@ ui <- fluidPage(
 server <- function(input, output) {
 
     output$mymap <- renderLeaflet({
-        
+      #Set YEAR with Slider
+      state_parole <- state_parole %>%
+        filter(year=input$year)
+      #Leaflet
+      leaflet(data = state_parole) %>%
+        addTiles()%>%
+        setView(lng = -80,
+                lat = 34.5,
+                zoom = 4) %>%
+        addPolygons(fillColor = ~pal(state_parole$number_on_parole_per_100000_us_adult_residents),
+                    fillOpacity = 1,
+                    color = "blue",
+                    opacity = 0.1,
+                    weight = 1,
+                    highlight = highlightOptions(
+                      weight = 3,
+                      color = "blue",
+                      fillOpacity = .2,
+                      bringToFront = TRUE),
+                    label = labels) %>%
+        addLegend(
+          position = "topright",
+          pal = pal,
+          values = ~number_on_parole_per_100000_us_adult_residents,
+          title = "Number of U.S. Adult Residents on Parole per 100000.",
+          opacity = 1)
        
         
         
